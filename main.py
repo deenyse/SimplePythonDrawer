bfb609e09f2adf4e954957a0b728c3561b5897f0
denvery31 <denvery31@gmail.com>
2022-10-16 16:23:51 +0300

    3: save source

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from tkinter import *
 import PIL
 from PIL import Image, ImageDraw
-from tkinter import messagebox
-from random import *
+from tkinter import filedialog as fd
 
 
-def save():
-    filename = f'image_{randint(0, 10000)}.png'
-    image1.save(filename)
-    messagebox.showinfo('Сохранение', 'Сохранено под названием %s' % filename)
+def save(*args):
+    source = fd.asksaveasfilename(initialfile='Untitled.png', defaultextension=".png")
+    image1.save(f'{source}')
 
 
 def activate_paint(event):
     x1, y1 = (event.x - 10), (event.y - 10)
     x2, y2 = (event.x + 10), (event.y + 10)
     cv.create_oval(x1, y1, x2, y2, fill='black', outline="black", width=0)
-    #draw.ellipse((x1, y1, x2, y2), fill='black', width=0)
+    draw.ellipse((x1, y1, x2, y2), fill='black', outline="black", width=0)
 
 
 root = Tk()
@@ -27,9 +25,9 @@ image1 = PIL.Image.new('RGB', (1280, 720), 'white')
 draw = ImageDraw.Draw(image1)
 
 cv.bind('<B1-Motion>', activate_paint)
+root.bind("S", save)
+
 cv.pack(expand=1, fill=BOTH)
 
-btn_save = Button(text="save", command=save, bg='black', fg='white', font=('Comic Sans MS', 30))
-btn_save.pack()
 
 root.mainloop()
