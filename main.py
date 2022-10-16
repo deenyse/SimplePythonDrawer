b6fc8125fb69dfde8b4d63c108bf06262764f516
denvery31 <denvery31@gmail.com>
2022-10-16 13:10:48 +0300

    1: test

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from tkinter import *
+import PIL
+from PIL import Image, ImageDraw
+from tkinter import messagebox
+from random import *
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def save():
+    filename = f'image_{randint(0, 10000)}.png'
+    image1.save(filename)
+    messagebox.showinfo('Сохранение', 'Сохранено под названием %s' % filename)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def activate_paint(event):
+    x1, y1 = (event.x - 10), (event.y - 10)
+    x2, y2 = (event.x + 10), (event.y + 10)
+    cv.create_oval(x1, y1, x2, y2, fill='black', outline="black", width=0)
+    #draw.ellipse((x1, y1, x2, y2), fill='black', width=0)
+
+
+root = Tk()
+root.resizable(width=False, height=False)
+
+cv = Canvas(root, width=1280, height=720, bg='white')
+
+image1 = PIL.Image.new('RGB', (1280, 720), 'white')
+draw = ImageDraw.Draw(image1)
+
+cv.bind('<B1-Motion>', activate_paint)
+cv.pack(expand=1, fill=BOTH)
+
+btn_save = Button(text="save", command=save, bg='black', fg='white', font=('Comic Sans MS', 30))
+btn_save.pack()
+
+root.mainloop()
